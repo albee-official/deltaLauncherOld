a313f5c817c471f329518dfe3d38b88363ca7a3e
Loli <sawukalu164@gmail.com>
2021-06-28 23:48:16 +0300

    Added main page and finished start page

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,13 @@ app.on('ready', () => {
     mainWindow = new BrowserWindow({
         width: 1400,
         height: 800,
+        minWidth: 824,
+        minHeight: 724,
         frame: false,
-        transparent: true,
-        vibrancy: 'fullscreen-ui',
+        thickFrame: true,
+        icon: 'D:/Projects/delta launcher wohoo/src/res/favicon.png',
+        // transparent: true,
+        // vibrancy: 'fullscreen-ui',
         webPreferences: {
             nodeIntegration: false,
             preload: path.join(__dirname, 'preload.js'),
@@ -33,6 +37,40 @@ app.on('ready', () => {
     });
 });
 
+function createMainWindow() {
+    return new Promise((resolve, _) => {
+        let win = new BrowserWindow({
+            width: 1400,
+            height: 800,
+            minWidth: 824,
+            minHeight: 724,
+            frame: false,
+            thickFrame: true,
+            icon: 'D:/Projects/delta launcher wohoo/src/res/favicon.png',
+            // transparent: true,
+            // vibrancy: 'fullscreen-ui',
+            webPreferences: {
+                nodeIntegration: false,
+                preload: path.join(__dirname, 'preload.js'),
+                contextIsolation: false,
+                enableRemoteModule: true,
+            },
+            show: false,
+        })
+    
+        win.loadFile('./src/pages/main/index.html');
+        win.on('ready-to-show', () => {
+            win.show()
+            resolve(win);
+        });
+    
+        win.webContents.on("devtools-opened", (err: string) => {
+            win.webContents.send("devtools-opened");
+            log.info("[MAIN] console opened");
+        });
+    })
+}
+
 // Biblioteks
 
 import { listeners } from  './includes/modpack-manager';
@@ -40,6 +78,12 @@ for (const listener_name in listeners) {
     //@ts-expect-error
     ipcMain.on(listener_name as string, listeners[listener_name] as Function);
 }
+
+ipcMain.on('open-main-window', async (event) => {
+    mainWindow = (await createMainWindow()) as BrowserWindow;
+    
+    event.reply('main-window-opened', event.sender.id)
+})
 
 ipcMain.on('get-window', (event) => {    
     event.reply('window-id', event.sender.id)
--- src/pages/start/js/renderer.ts
+++ src/pages/start/js/renderer.ts
@@ -15,11 +15,94 @@ declare let ipcRenderer: IpcRenderer;
 declare let modpackManager: any;
 //#endregion
 
-console.log('> [START] Hallo from render :)');
+console.log('> [START] hello from render :)');
 
 const coreCount = document.getElementById('cores');
-// console.log(modpackManager);
+const start_steps = document.getElementById('steps')?.children;
+
+function showLogin() {
+    if (start_steps) start_steps[0].classList.remove('active');
+    if (start_steps) start_steps[1].classList.add('active');
+
+    document.getElementById('bg')?.classList.remove('darken');
+    document.body.classList.add('login-open');
+}
+
+function closeLogin() {
+    if (start_steps) start_steps[1].classList.remove('active');
+    if (start_steps) start_steps[2].classList.add('active');
+
+    document.getElementById('bg')?.classList.add('darken');
+    document.body.classList.remove('login-open');
+}
+
+let a = 1;
 
 (async () => {
-    
+    // speedrun();
+
+    await checkForUpdates();
+
+    showLogin();
+    document.getElementById('login-submit')?.addEventListener('click', async (e) => {
+        e.preventDefault();
+        await login();
+        closeLogin();
+
+        await start();
+    });
 })();
+
+async function speedrun() {
+    a = .01;
+    await checkForUpdates();
+    await login();
+    await start();
+}
+
+let procent = 0;
+async function checkForUpdates() {
+    console.log('> [START] checking for updates...');
+    
+    // delay in async method
+    procent = 0;
+    let ae = setInterval(() => {
+        procent++;
+        if (start_steps) start_steps[0].innerHTML = `Проверка обновлений: ${procent}%`
+    }, 2000 / 100);
+    await new Promise((resolve, reject) => { setTimeout(() => {resolve(undefined)}, 2000 * a) } );
+    clearInterval(ae);
+}
+
+async function login() {
+    console.log('> [START] logging you in...');
+
+    // delay in async method
+    procent = 0;
+    let ae = setInterval(() => {
+        procent++;
+        if (start_steps) start_steps[1].innerHTML = `Запрос авторизации: ${procent}%`
+    }, 1000 / 100);
+    await new Promise((resolve, reject) => { setTimeout(() => {resolve(undefined)}, 1000 * a) } );
+    clearInterval(ae);
+}
+
+async function start() {
+    console.log('> [START] starting...');
+
+    // delay in async method
+    procent = 0;
+    let ae = setInterval(() => {
+        procent++;
+        if (start_steps) start_steps[2].innerHTML = `Запуск: ${procent}%`
+    }, 3000 / 100);
+    await new Promise((resolve, reject) => { setTimeout(() => {resolve(undefined)}, 3000 * a) } );
+    clearInterval(ae);
+
+    ipcRenderer.send('open-main-window');
+    
+    setTimeout(() => {
+        //@ts-expect-error
+        browserWindow.exit();
+    }, 250);
+}
